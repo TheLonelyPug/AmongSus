090a54f90725a49b6d18dcb797e4d7a3219fa88d
TheLonelyPug <assasinsgir@gmail.com>
2021-07-07 01:13:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const config = require('./config.json');
-var Discord = require('discord.js');
+const config = require("./config.json");
+var Discord = require("discord.js");
 var client = new Discord.Client();
 var isReady = true;
 
-
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-client.user.setPresence({ activity: { name: 'Red kinda sus' }, status: 'dnd' })
-})
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    activity: { name: "Red kinda sus" },
+    status: "dnd",
+  });
+});
 
-client.on('message', message => {
-  if (isReady && message.content === '!sus')
-  {
-  isReady = false;
-  var voiceChannel = message.member.voiceChannel;
-  message.member.voice.channel.join()
-  .then(connection =>
-  {
-     const dispatcher = connection.play('./audio/sus.mp3');
-     dispatcher.on("finish", end => {
-       message.member.voice.channel.leave()
-       });
-   }).catch(err => console.log(err));
-   isReady = true;
+client.on("message", (message) => {
+  if (isReady && message.content === "!sus") {
+    isReady = false;
+    var voiceChannel = message.member.voiceChannel;
+    message.member.voice.channel
+      .join()
+      .then((connection) => {
+        const dispatcher = connection.play("./audio/sus.mp3");
+        dispatcher.on("finish", (end) => {
+          message.member.voice.channel.leave();
+        });
+      })
+      .catch((err) => console.log(err));
+    isReady = true;
   }
 });
 
